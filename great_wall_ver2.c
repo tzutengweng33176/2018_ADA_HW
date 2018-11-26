8ca408117701237c1a9da98aa3fce88af8ba6f3e
Tzu-teng Weng <tzutengweng33176@gmail.com>
2018-11-26 11:24:38 +0800

    great_wall_ver_2 changed

--- great_wall_ver2.c
+++ great_wall_ver2.c
@@ -7,7 +7,8 @@ using namespace std;
 // An interval has start time and end time 
 struct Interval 
 { 
-   long long int start, end; 
+   long long int start, end;
+   long long int back_pt=0; 
 }; 
 
 long long int power_of_interval(Interval i)
@@ -48,25 +49,23 @@ vector<Interval> insert(vector<Interval> &input, Interval given, long long int p
     if (sz == 0)
     {
         result.push_back(given);
-				power[day]=power_of_interval(given);
+		power[day]=power_of_interval(given);
         return result;
     }
     // Case : given comes at the beginning or at the end without overlap 
     if (given.start > input[sz - 1].end)
     {
-       // if (given.end < input[0].start)
-       // {
-       //     result.push_back(given);
-       // }
+    
         for (long long int i = 0; i < sz; i++)
         {
             result.push_back(input[i]);
         }
         if (given.start > input[sz - 1].end)
         {
-            result.push_back(given);
+           given.back_pt=day; 
+					 result.push_back(given);
         }
-				power[day]=power[day-1]+power_of_interval(given);
+		power[day]=power[day-1]+power_of_interval(given);
         return result;
     }
 
@@ -76,34 +75,18 @@ vector<Interval> insert(vector<Interval> &input, Interval given, long long int p
         if (!intersect)  // if NOT overlap
         {
             result.push_back(input[i]);
-            // check if given lies between input[i] and input[i+1]
-       //     if (i < sz - 1 && given.start > input[i].end && given.end < input[i+1].start)
-       //     {
-       //         result.push_back(given);
-			//					power[day]+=power_of_interval(given);
-       //     }
-				//		power[day]+=power_of_interval(input[i]);
+
             continue;
         }else{
         // Now we know the pair overlaps. Lets find out how many input overlap. 
         if(i!=0){
-				power[day]+=power[i-1];
-				}
+				power[day]+=power[input[i-1].back_pt];
+			}
 
-					long long int st = i; 
-     //   while (i < sz && intersect)
-     //   {
-     //       i++;
-     //       if (i == sz) 
-     //           intersect = false; 
-     //       else
-     //       {
-     //           intersect = doesIntersect(input[i], given);
-     //       }
-     //   }
-     //   i--;
+		long long int st = i; 
+
         // Now all input from st to i overlap. 
-        Interval toInsert{min(given.start, input[st].start), given.end};
+        Interval toInsert{min(given.start, input[st].start), given.end, day};
         result.push_back(toInsert);
 				power[day]+=power_of_interval(toInsert);
 				break;
@@ -136,11 +119,7 @@ for(long long int i=0; i<n; i++){
 vector<Interval> vect;
 //traverse the graph from vertex 0
 for(long long int i=0; i<n; i++){
-// sort the intervals in increasing order of start time 
-// if the vector is NOT empty
-//  if(!vect.empty()){ 
-//	sort(vect.begin(), vect.end(), compareInterval);
-//	}
+
   vect=insert(vect, arr[i], power, i);
 
 
